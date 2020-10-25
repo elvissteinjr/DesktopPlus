b6fafd5f113292704e9cd9a938116eb30346686d
elvissteinjr <alex.ruediger@googlemail.com>
2020-10-25 20:20:49 +0100

    Bypass execution policy when running the CreateElevatedTask PowerShell script

--- assets/misc/CreateElevatedTask.bat
+++ assets/misc/CreateElevatedTask.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo Launching script with administrator privileges...
-powershell -command "   Start-Process PowerShell -Verb RunAs \""-Command `\""cd '%cd%'; & '.\CreateElevatedTask.ps1';`\""\""   "
+powershell -command "   Start-Process PowerShell -Verb RunAs \""-ExecutionPolicy Bypass -Command `\""cd '%cd%'; & '.\CreateElevatedTask.ps1';`\""\""   "
