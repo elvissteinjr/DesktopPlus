90f85907f75a215859b8ebe520ca83078fd25861
elvissteinjr <alex.ruediger@googlemail.com>
2022-03-20 03:25:47 +0100

    Bump version to NewUI Preview 5

--- src/DesktopPlusUI/WindowSettings.cpp
+++ src/DesktopPlusUI/WindowSettings.cpp
@@ -1131,7 +1131,7 @@ void WindowSettings::UpdatePageMainCatMisc()
         ImGui::Columns(2, "ColumnVersion", false);
         ImGui::SetColumnWidth(0, m_Column0Width);
 
-        ImGui::TextUnformatted("Desktop+ NewUI Preview 4");
+        ImGui::TextUnformatted("Desktop+ NewUI Preview 5");
 
         ImGui::Columns(1);
     }
