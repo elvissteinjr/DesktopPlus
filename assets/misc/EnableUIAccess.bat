e4aad8cc38272ee091585c1db87adb362731cbb2
elvissteinjr <alex.ruediger@googlemail.com>
2022-11-03 04:30:28 +0100

    Fix misc batch files not running the powershell scripts properly when running as admin & check for file/task presence to avoid error messages on success in UndoSystemChanges.ps1
    CMD changes the working directory when running batch files as admin, so we always explicitly set it instead.

--- assets/misc/EnableUIAccess.bat
+++ assets/misc/EnableUIAccess.bat
@@ -1,4 +1,5 @@
 @echo off
 
+pushd %~dp0
 echo Launching script with administrator privileges...
 powershell -command "   Start-Process PowerShell -Verb RunAs \""-ExecutionPolicy Bypass -Command `\""cd '%cd%'; & '.\EnableUIAccess.ps1';`\""\""   "
