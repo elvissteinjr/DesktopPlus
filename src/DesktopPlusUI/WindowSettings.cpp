bb04ccbaa5082486a4470985b325b5953bbe9c6d
elvissteinjr <alex.ruediger@googlemail.com>
2023-12-13 03:57:03 +0100

    Bump version to 2.8.5

--- src/DesktopPlusUI/WindowSettings.cpp
+++ src/DesktopPlusUI/WindowSettings.cpp
@@ -3105,7 +3105,7 @@ void WindowSettings::UpdateCatMisc()
         ImGui::Columns(2, "ColumnVersionInfo", false);
         ImGui::SetColumnWidth(0, column_width_0 * 2.0f);
 
-        ImGui::Text("Desktop+ Version 2.8.4");
+        ImGui::Text("Desktop+ Version 2.8.5");
 
         ImGui::Columns(1);
     }
