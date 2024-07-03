10178df620dbf2781209006cd35241f3ed6bba5a
elvissteinjr <alex.ruediger@googlemail.com>
2024-07-03 02:42:11 +0200

    Fix logging version string always using the nightly build variant

--- src/Shared/Logging.h
+++ src/Shared/Logging.h
@@ -6,11 +6,8 @@
 
 //Version string logged and displayed in the UI
 //DPLUS_SHA is set externally for nightly builds to use the commit hash in version string
-#ifdef DPLUS_SHA
-	static const char* const k_pch_DesktopPlusVersion = "Desktop+ NewUI " DPLUS_SHA;
-#else
-	static const char* const k_pch_DesktopPlusVersion = "Desktop+ NewUI Preview 14";
-#endif
+//DPLUS_SHA is also always defined but usually empty... so we detect this with some constexpr trickery instead of messing with the build system even more
+constexpr const char* const k_pch_DesktopPlusVersion = (sizeof("" DPLUS_SHA) <= 1) ? "Desktop+ NewUI Preview 14" : "Desktop+ NewUI " DPLUS_SHA;
 
 //Version written to config file
 //Only really increased when backward incompatible changes were made (we fall back to default values when things are missing usually)
